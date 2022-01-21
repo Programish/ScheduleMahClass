b3d9e3dabe58a92f79dab47cea52ce2f31feeb06
Programish <harshit12kumar34@gmail.com>
2022-01-21 13:22:32 +0530

    First Release

---
+++ color.py
@@ -0,0 +1,30 @@
+import colorama
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+#ScheduleMahClass    
+def Start():
+    print(Fore.RED + Style.BRIGHT + '   _____     ', end='') 
+    print(Fore.GREEN + '      ______')
+    print(Fore.YELLOW + Style.BRIGHT + '  / ___/____', end='')
+    print(Fore.MAGENTA + Style.BRIGHT + ' ___  / ____/')
+    print(Fore.WHITE + Style.BRIGHT + '  \__ \/ __ ', end='')
+    print(Fore.CYAN + Style.BRIGHT + '`__ \/ /     ')
+    print(Fore.RED + Style.BRIGHT + ' ___/ / / /', end='') 
+    print(Fore.GREEN + ' / / / /___   ') 
+    print(Fore.YELLOW + Style.BRIGHT + '/____/_/ /_', end='')
+    print(Fore.MAGENTA + Style.BRIGHT + '/ /_/\____/')
+
+#EndOfDay
+def End():
+    print(Fore.RED + Style.BRIGHT + '    ______   ', end='') 
+    print(Fore.GREEN + '   ____ ')
+    print(Fore.YELLOW + Style.BRIGHT + '   / ____/___', end='')
+    print(Fore.MAGENTA + Style.BRIGHT + '  / __ \\')
+    print(Fore.WHITE + Style.BRIGHT + '  / __/ / __ \\', end='')
+    print(Fore.CYAN + Style.BRIGHT + '/ / / /')
+    print(Fore.RED + Style.BRIGHT + ' / /___/ /_/', end='') 
+    print(Fore.GREEN + ' / /_/ / ') 
+    print(Fore.YELLOW + Style.BRIGHT + '/_____/\____', end='')
+    print(Fore.MAGENTA + Style.BRIGHT + '/_____/')
--- main.py
+++ main.py
@@ -2,40 +2,53 @@ import pandas as pd
 import webbrowser
 from datetime import datetime, timedelta
 from time import sleep
+from color import *
 
 df_link = pd.read_csv('assets/Subject_Links.csv', index_col=0)
-#webbrowser.open_new_tab(df['Links']['Subject_Name'])
 
 df_sub = pd.read_csv('assets/Time_Table.csv', index_col='WeekDays')
 #print(df_sub.info())
 
 def handler(sub):
     
-    if sub not in ('CLC', 'X'): 
-        webbrowser.open_new_tab(df_link['Links'][sub])
-        print(sub + 'class is started!!')
-    #    if 'meet' in df_link['Links'][sub]:
-            
+    if sub != 'X':
+
+        if sub not in ('CLC', 'SE'): 
+            webbrowser.open_new_tab(df_link['Links'][sub])
+            print(sub + ' class is started!!')
+        #   if 'meet' in df_link['Links'][sub]:
+           
+        else:
+            print('[PANIC] YOU NEED TO JOIN "' + sub + '" CLASS MANUALLY !!!!')
     else:
         t = datetime.now().strftime('%H:%M:%S')
         print('No class at ' + t)
 
 def wait_and_call(time, cb, sub):
-        
+       
+    # Wait until class starts. 
     while datetime.now() < time:
         sleep(2)
 
+    # If you're late for the class, and the class is still
+    # going on then this 'if' condition helps to join class
+    # immediately otherwise ignore.
     if datetime.now().strftime('%H') == time.strftime('%H'):
-        cb(sub)
+        cb(sub)#Starting class of that time
 
 def main():
-    
     Today_num = datetime.today().weekday()
     sub = df_sub.iloc[Today_num]
-    today_time = datetime.now().replace(hour=8, minute=0, second=0, microsecond=0)
+    
+    # Initial starting time of today's meeting
+    today_time = datetime.now().replace(hour=8, minute=0, second=0,
+            microsecond=0)
+    
     for subject in sub:
         wait_and_call(today_time, handler, subject)
         today_time += timedelta(hours=1)
 
 if __name__ == '__main__':
+    Start()
     main()
+    End()
